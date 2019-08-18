b8b700e50cac0971448ec469ecc8dd1bb57b1b70
Dmitriy <dima.abdurazakov.92@gmail.com>
2019-08-19 00:14:24 +0300

    зачатки логгера

--- src/main/java/ru/pflb/homework/elementModels/Button.java
+++ src/main/java/ru/pflb/homework/elementModels/Button.java
@@ -2,23 +2,19 @@ package ru.pflb.homework.elementModels;
 
 import org.openqa.selenium.WebElement;
 import ru.pflb.homework.interfaces.Clickable;
-
-import java.util.function.Consumer;
+import ru.pflb.homework.utils.CustomLogger;
 
 public class Button extends AbstractElement implements Clickable {
-    private Consumer<Button> onClick;
 
     public Button(WebElement initialElement, String triggerName) {
         super(initialElement);
         if (triggerName != null && !triggerName.equals("")) {
-//            this.onClick = Triggers.from(triggerName);
         }
     }
 
     @Override
     public void click() {
-
+        CustomLogger.info(String.format("Клик по элементу '%s'", getInitialElement().getText()));
         initialElement.click();
-        onClick.accept(this);
     }
 }
--- src/main/java/ru/pflb/homework/elementModels/Label.java
+++ src/main/java/ru/pflb/homework/elementModels/Label.java
@@ -3,6 +3,7 @@ package ru.pflb.homework.elementModels;
 import org.openqa.selenium.WebElement;
 import ru.pflb.homework.interfaces.Clickable;
 import ru.pflb.homework.interfaces.HasText;
+import ru.pflb.homework.utils.CustomLogger;
 
 public class Label extends AbstractElement implements HasText, Clickable {
     public Label(WebElement initialElement) {
@@ -16,6 +17,7 @@ public class Label extends AbstractElement implements HasText, Clickable {
 
     @Override
     public void click() {
+        CustomLogger.info(String.format("Клик по элементу '%s'", getInitialElement().getText()));
         initialElement.click();
     }
 }
